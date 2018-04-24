d1159fbb8451e53d1b473c4b4a4af632f7636f65
Tomasz Lemkowski <lemkowski@gmail.com>
2018-04-24 18:33:25 +0200

    update to latest version

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const path = require('path');
-
-const isEmpty = input => (input.length === 0 ? console.log(chalk.red('\nCant be empty')) : true);
+const targetPath = path.join(process.cwd(), 'marketplace_builder');
 
 module.exports = class extends Generator {
   prompting() {
@@ -10,21 +9,51 @@ module.exports = class extends Generator {
       {
         type: 'input',
         name: 'blog_scope',
-        message: 'Your blog slug (ie. blog):',
-        default: 'new-blog',
-        validate: isEmpty
+        message: 'Your blog url:',
+        default: 'new-blog'
+      },
+      {
+        type: "list",
+        name: "blog_type",
+        message: "Your blog type is:",
+        choices: [ "User Blog", "Instance Blog" ]
+      },
+      {
+        type: 'input',
+        name: 'layout_name',
+        message: 'Your blog layout name:',
+        default: 'new-blog'
       }
     ];
 
-    return this.prompt(prompts).then(props => (this.props = props));
+    return this.prompt(prompts).then(props => {
+      this.props = props;
+    });
   }
 
   writing() {
-    this.fs.copyTpl(this.templatePath('.'), this.destinationPath('../../..'), this.props);
+    this.fs.copyTpl(this.templatePath(".") + '/common', this.destinationPath(path.join(process.cwd())), this.props);
+    this.fs.move(targetPath + '/liquid_views/layouts/blog.liquid',
+      targetPath + '/liquid_views/layouts/' + this.props.layout_name + '.liquid')
+    this.fs.move(targetPath + '/pages/blog/**',
+      targetPath + '/pages/' + this.props.blog_scope)
+
+    if (this.props.blog_type == 'User Blog') {
+      this.fs.copyTpl(this.templatePath(".") + '/user_blog', this.destinationPath(path.join(process.cwd())), this.props);
+    } else {
+      this.fs.copyTpl(this.templatePath(".") + '/instance_blog', this.destinationPath(path.join(process.cwd())), this.props);
+    }
+  }
+
+  install() {
+    console.log(chalk.green('MPP :: Blog :: Installing'));
+    // console.log(chalk.green("MPP :: Blog :: Installing NPM dependencies"));
+
+    // process.chdir(`${process.cwd()}/${this.projectDir}`);
+    // this.npmInstall();
   }
 
   end() {
-    console.log(chalk.green('Blog :: Installing'));
-    console.log(chalk.green('Blog :: Module files generated'));
+    console.log(chalk.green('MPP :: BlogModule :: Module files generated'));
   }
 };
